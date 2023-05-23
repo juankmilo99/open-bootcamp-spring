607626baa9ff750b2dafec4f8410dbd93f25fb8b
juankmilo99 <43284555+juankmilo99@users.noreply.github.com>
2023-05-23 16:54:31 -0500

    ejercicio 3

---
+++ open-bootcamp-spring-ej3/src/main/java/juankmilo/spring/openbootcampspringej3/OpenBootcampSpringEj3Application.java
@@ -0,0 +1,13 @@
+package juankmilo.spring.openbootcampspringej3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenBootcampSpringEj3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenBootcampSpringEj3Application.class, args);
+	}
+
+}
---
+++ open-bootcamp-spring-ej3/src/test/java/juankmilo/spring/openbootcampspringej3/OpenBootcampSpringEj3ApplicationTests.java
@@ -0,0 +1,13 @@
+package juankmilo.spring.openbootcampspringej3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenBootcampSpringEj3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
